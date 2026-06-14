83764ba417574aff2a8d5e842edfca7236a34912
Twesi-001 <2023bse147@std.must.ac.ug>
2026-06-14 13:05:07 +0300

    adding admin route to App.tsx

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,29 +1,27 @@
 import { Routes, Route } from 'react-router-dom';
 import Header from '../components/ui/Header';
 import Footer from '../components/ui/footer';
-import Home from './Home';
-import About from './About';
-import Products from './Products';
-import Contact from './Contact';
-import Socials from './Socials';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Products from '../pages/Products';
+import Contact from '../pages/Contact';
+import Socials from '../pages/Socials';
 import AdminDashboard from '../pages/AdminDashboard';
 
 function App() {
   return (
-    <div className="app-container">
+    <>
       <Header />
-      <main className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/social" element={<Socials />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-        </Routes>
-      </main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/social" element={<Socials />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+      </Routes>
       <Footer />
-    </div>
+    </>
   );
 }
 
