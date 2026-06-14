7ce4839ec883c0819938945a4cda2a739bbeeac2
Twesi-001 <2023bse147@std.must.ac.ug>
2026-06-14 12:29:17 +0300

    working on Admin

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Header from '../components/ui/Header';
-import Footer from '../components/ui/Footer';
+import Footer from '../components/ui/footer';
 import Home from './Home';
 import About from './About';
 import Products from './Products';
 import Contact from './Contact';
 import Socials from './Socials';
+import AdminDashboard from '../pages/AdminDashboard';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/products" element={<Products />} />
           <Route path="/social" element={<Socials />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/admin" element={<AdminDashboard />} />
         </Routes>
       </main>
       <Footer />
